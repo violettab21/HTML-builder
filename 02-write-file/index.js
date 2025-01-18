1be41c7f138dcf92a621fbabfe0a36630b55099f
violettab <violettabatsura@gmail.com>
2025-01-18 22:21:59 +0300

    feat: implement Writin Console Input to File task

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const { stdin, stdout } = process;
+
+const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+stdout.write('Input content:\n');
+stdin.on('data', (data) => {
+  let input = String(data);
+  if (input.trim() !== 'exit') {
+    output.write(input);
+  } else process.exit();
+});
+process.on('exit', () =>
+  stdout.write(`Entering is finished, input is saved into text.txt!`),
+);
+process.on('SIGINT', () => {
+  process.exit();
+});
