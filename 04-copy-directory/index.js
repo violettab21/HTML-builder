a120910f819b6013354e649e6903f2ac62477b15
violettab <violettabatsura@gmail.com>
2025-01-19 02:12:50 +0300

    refactor: move logic to copyDir function in Copying a Directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,41 +1,41 @@
 const fs = require('fs');
 const path = require('path');
+copyDir('files', 'files-copy');
+function copyDir(from, to) {
+  fs.mkdir(path.join(__dirname, to), { recursive: true }, (err) => {
+    if (err) console.log(err);
+  });
 
-fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
-  if (err) console.log(err);
-});
-
-fs.readdir(path.join(__dirname, 'files'), (err, files) => {
-  if (err) console.log(err);
-  else {
-    let filesOrigin = [];
-    files.forEach((file) => {
-      filesOrigin.push(file);
-      fs.copyFile(
-        path.join(__dirname, 'files', file),
-        path.join(__dirname, 'files-copy', file),
-        (err) => {
-          if (err) console.log(err);
-        },
-      );
-    });
-    console.log(filesOrigin);
-    fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
-      if (err) console.log(err);
-      else {
-        let filesCopied = [];
-        files.forEach((file) => {
-          filesCopied.push(file);
-        });
-        filesCopied.forEach((el) => {
-          if (filesOrigin.indexOf(el) === -1) {
-            console.log(el);
-            fs.rm(path.join(__dirname, 'files-copy', el), (err) => {
-              if (err) console.log(err);
-            });
-          }
-        });
-      }
-    });
-  }
-});
+  fs.readdir(path.join(__dirname, from), (err, files) => {
+    if (err) console.log(err);
+    else {
+      let filesOrigin = [];
+      files.forEach((file) => {
+        filesOrigin.push(file);
+        fs.copyFile(
+          path.join(__dirname, from, file),
+          path.join(__dirname, to, file),
+          (err) => {
+            if (err) console.log(err);
+          },
+        );
+      });
+      fs.readdir(path.join(__dirname, to), (err, files) => {
+        if (err) console.log(err);
+        else {
+          let filesCopied = [];
+          files.forEach((file) => {
+            filesCopied.push(file);
+          });
+          filesCopied.forEach((el) => {
+            if (filesOrigin.indexOf(el) === -1) {
+              fs.rm(path.join(__dirname, to, el), (err) => {
+                if (err) console.log(err);
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+}
