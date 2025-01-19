6d1d076ef6bc6dd20de41cabbbaa5505d8cca10e
violettab <violettabatsura@gmail.com>
2025-01-19 21:17:36 +0300

    feat: implement Building an HTML Page from Components and Style task

---
+++ 06-build-page/index.js
@@ -0,0 +1,123 @@
+const fs = require('fs');
+const path = require('path');
+
+//create folder
+
+fs.mkdir(path.join(__dirname, 'project-dist'), { recursive: true }, (err) => {
+  if (err) console.log(err);
+});
+
+//read the content of template
+
+let input = fs.createReadStream(path.join(__dirname, 'template.html'), 'utf-8');
+/*const output = fs.createWriteStream(
+  path.join(__dirname, 'project-dist', 'index.html'),
+);*/
+
+let template = '';
+input.on('data', (chunk) => (template += chunk));
+input.on('end', () => {
+  let re = /(?<=\{{).*?(?=\}})/gi;
+  //find tags in the template
+  let componentsForInsert = template.match(re);
+  //read content of components files and replace template tag with content, write in the file
+  componentsForInsert.forEach((component) => {
+    let componentFileInput = fs.createReadStream(
+      path.join(__dirname, 'components', `${component}.html`),
+      'utf-8',
+    );
+    let componentFileData = '';
+    componentFileInput.on('data', (chank) => (componentFileData += chank));
+    componentFileInput.on('end', () => {
+      template = template.replace(`{{${component}}}`, componentFileData);
+      fs.writeFile(
+        path.join(__dirname, 'project-dist', 'index.html'),
+        template,
+        (err) => {
+          if (err) console.log(err);
+        },
+      );
+    });
+  });
+});
+
+//create css
+const outputStyle = fs.createWriteStream(
+  path.join(__dirname, 'project-dist', 'style.css'),
+);
+
+fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      fs.stat(path.join(__dirname, 'styles', file), (err, stats) => {
+        if (err) console.log(err);
+        else {
+          if (stats.isFile() && path.extname(file) === '.css') {
+            const inputStyle = fs.createReadStream(
+              path.join(__dirname, 'styles', file),
+            );
+            inputStyle.on('data', (chunk) => outputStyle.write(chunk));
+          }
+        }
+      });
+    });
+  }
+});
+
+//copy files from assets
+
+function createFolder(destination) {
+  fs.mkdir(destination, { recursive: true }, (err) => {
+    if (err) console.log(err);
+  });
+}
+
+function copyFolderContent(from, to) {
+  fs.readdir(path.join(__dirname, ...from), (err, files) => {
+    if (err) console.log(err);
+    else {
+      let filesOrigin = [];
+      files.forEach((file) => {
+        filesOrigin.push(file);
+        fs.copyFile(
+          path.join(__dirname, ...from, file),
+          path.join(__dirname, ...to, file),
+          (err) => {
+            if (err) console.log(err);
+          },
+        );
+      });
+      console.log(filesOrigin);
+      fs.readdir(path.join(__dirname, ...to), (err, files) => {
+        if (err) console.log(err);
+        else {
+          let filesCopied = [];
+          files.forEach((file) => {
+            filesCopied.push(file);
+          });
+          filesCopied.forEach((el) => {
+            if (filesOrigin.indexOf(el) === -1) {
+              console.log(el);
+              fs.rm(path.join(__dirname, ...to, el), (err) => {
+                if (err) console.log(err);
+              });
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+createFolder(path.join(__dirname, 'project-dist', 'assets'));
+
+fs.readdir(path.join(__dirname, 'assets'), (err, files) => {
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      createFolder(path.join(__dirname, 'project-dist', 'assets', file));
+      copyFolderContent(['assets', file], ['project-dist', 'assets', file]);
+    });
+  }
+});
